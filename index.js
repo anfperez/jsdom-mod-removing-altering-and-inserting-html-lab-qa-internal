2ffe2456bde4c5dd792a3d5ff56c4ff7e8f32eb8
Angela Perez <angela.perez@flatironschool.com>
2018-08-21 21:42:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-/* Enter the code to remove the main node element under this comment */
+console.log('this works!');
+
+/* Enter the code to remove the node element here */
+
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
